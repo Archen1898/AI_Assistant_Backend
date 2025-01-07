e3275dbadce5373938ef3dd4463ed8dafc90f6bd
Dillon Archer <132870045+Archen1898@users.noreply.github.com>
2025-01-06 22:57:55 -0500

    Added error messages for ExpenseType

---
+++ AI_Assistant_Backend/lang/en/expense_types.php
@@ -0,0 +1,13 @@
+<?php
+
+return[
+    'index' => 'Expense Types Successfully fetched',
+    'showById' => 'Expense Type Successfully fetched',
+    'add' => 'Expense Type Successfully created',
+    'update' => 'Expense Type Successfully updated',
+    'delete' => 'Expense Type Successfully deleted',
+    'notFound' => 'Expense Type not found',
+    'alreadyExists' => 'Expense Type already exists',
+    'notFoundById' => 'Expense Type not found with that id',
+    'error'=> 'Something went wrong, please try again later'
+];
