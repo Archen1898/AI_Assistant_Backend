32456991d69fc627f2f5c1091807c29f9030b82d
Dillon Archer <132870045+Archen1898@users.noreply.github.com>
2025-01-08 13:40:10 -0500

    Create IncomeTypeController.php

---
+++ AI_Assistant_Backend/app/Http/Controllers/IncomeTypeController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//Global Import
+
+use App\Http\Requests\IncomeTypeRequest;
+use App\Models\Income;
+use Exception;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+//Local Import
+use App\Traits\ResponseTrait;
+use App\Repositories\IncomeTypeRepository;
+
+
+class IncomeTypeController extends Controller
+{
+    protected IncomeTypeRepository $incomeTypeRepository;
+    use ResponseTrait;
+    public function __construct(IncomeTypeRepository $incomeTypeRepository)
+    {
+        $this->incomeTypeRepository = $incomeTypeRepository;
+    }
+
+    public function indexIncomeTypes()
+    {
+        try {
+            return $this->response(Response::HTTP_OK, trans('income_types.index'), $this->incomeTypeRepository->viewAll(), null);
+        } catch (Exception $exception) {
+            return $this->response(Response::HTTP_BAD_REQUEST, $exception->getMessage(), [], $exception->getMessage());
+        }
+    }
+
+    public function indexIncomeTypesByStatus($status)
+    {
+        try {
+            return $this->response(Response::HTTP_OK, trans('income_types.index'), $this->incomeTypeRepository->viewAllByStatus($status), null);
+        } catch (Exception $exception) {
+            return $this->response(Response::HTTP_BAD_REQUEST, $exception->getMessage(), [], $exception->getMessage());
+        }
+    }
+
+    public function createIncomeType(IncomeTypeRequest $request)
+    {
+        try {
+            return $this->response(Response::HTTP_OK, trans('income_types.created'), $this->incomeTypeRepository->create($request->all()), null);
+        } catch (Exception $exception) {
+            return $this->response(Response::HTTP_BAD_REQUEST, $exception->getMessage(), [], $exception->getMessage());
+        }
+    }
+
+    public function updateIncomeType(IncomeTypeRequest $request, int $id)
+    {
+        try {
+            return $this->response(Response::HTTP_OK, trans('income_types.updated'), $this->incomeTypeRepository->update($id, $request->all()), null);
+        } catch (Exception $exception) {
+            return $this->response(Response::HTTP_BAD_REQUEST, $exception->getMessage(), [], $exception->getMessage());
+        }
+    }
+}
